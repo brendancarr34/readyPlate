15c3847b48b8bfcf731f84bd182ab4fb02c912bb
Jack Decker <Jack@Deckers-MacBook-Pro.local>
2020-10-13 18:41:36 -0400

    Updates

---
+++ precache-manifest.ba6e4e46323212f700f609c7bf24e072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7468b7e4342d048dd49417b716ec482e",
+    "url": "/MealScheduler/index.html"
+  },
+  {
+    "revision": "558efbd38fbef16ec273",
+    "url": "/MealScheduler/static/css/main.6cccb1d3.chunk.css"
+  },
+  {
+    "revision": "a40062a3da541c66c0c3",
+    "url": "/MealScheduler/static/js/2.d9487bd2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MealScheduler/static/js/2.d9487bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558efbd38fbef16ec273",
+    "url": "/MealScheduler/static/js/main.6345f564.chunk.js"
+  },
+  {
+    "revision": "62a16748e774ce115a20",
+    "url": "/MealScheduler/static/js/runtime-main.1d6f3af5.js"
+  },
+  {
+    "revision": "9acfe78b8e1cfbf4c1b1d1f31745b96b",
+    "url": "/MealScheduler/static/media/profilepic.9acfe78b.png"
+  }
+]);

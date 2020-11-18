c8221192c69fed9581c2a273c5300f4d4a63c811
Jack Decker <jackdeckere@gmail.com>
2020-11-17 22:10:55 -0500

    Added date picker

--- src/Main.js
+++ src/Main.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './index.css';
 import Header from './Header';
 import DayCard from './DayCard';
+import DateRangePicker from 'react-bootstrap-daterangepicker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-daterangepicker/daterangepicker.css';
+import './Main.css';
+
 
 
 let Main = () => {
+  let date = new Date();
     return (  
     <div>
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '70vh'}}>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '30vh'}}>
+        <DateRangePicker initialSettings={{ startDate: (date.getMonth()+1) + '/'+ date.getDate() +'/' + date.getFullYear()}}>
+          <button class="material-button"type="button"><span>Pick Dates</span></button>
+        </DateRangePicker>
+      </div>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '30vh'}}>
          <DayCard/>
       </div>
     </div>

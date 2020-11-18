2f3723cd1e99093f7159a761fb2b9ff0ae56e822
Jack Decker <jackdeckere@gmail.com>
2020-11-17 19:56:22 -0500

    Integrated backend in DayCard component

--- src/index.js
+++ src/index.js
@@ -4,26 +4,16 @@ import './index.css';
 import Header from './Header';
 import DayCard from './DayCard';
 import * as serviceWorker from './serviceWorker';
-let dayCard;
 
-
-async function componentDidMount() {
-  dayCard = await DayCard().renderDom();
-}
-
-let renderDom = async function() {
-  ReactDOM.render(
+ReactDOM.render(
     <React.StrictMode>
       <Header/>
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-         {await dayCard}
+         <DayCard/>
       </div>
     </React.StrictMode>,
     document.getElementById('root')
   );
-}
-
-componentDidMount();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

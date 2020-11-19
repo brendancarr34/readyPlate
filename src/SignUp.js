b8104082d993d50b1fbf84bd9451df6073167f63
brendo <brendo@brendo-pc.local>
2020-11-18 21:19:01 -0500

    login & signup added

---
+++ src/SignUp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+
+
+function SignUp () {
+    return (
+        <div>
+            <h1>
+                SIGN UP!
+            </h1>
+            <Button style={{padding:'50px'}}>
+                SIGN UP!
+            </Button>
+        </div>
+        
+    );
+}
+
+export default SignUp;

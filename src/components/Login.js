e479fc657cc74e5f7192802dd477bf54d7896b25
Jack Decker <jackdeckere@gmail.com>
2020-11-20 15:01:00 -0500

    Fixed login page layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import Image from 'react-bootstrap/Image';
-import Form from 'react-bootstrap/Form'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
+import {Image, Form, Col, Row, Container, Button, Jumbotron } from 'react-bootstrap';
 
 
 function Login () {
     return (
-        <div height='100vh'>
-            <Container fluid style={{height:'100vh'}}>
-                <Row s={2} >
-                    <Col xs={8} style={{backgroundColor: '#f47373', justifyContent:'center'}}>
-                        <div style={{height:'100vh',marginTop: '5%'}}>
+        <div>
+            <Container fluid style={{backgroundColor: '#f47373', height: '100vh', display: 'flex'}}>
+                <Row s={2} style={{flex: '1'}}>
+                    <Col md style={{justifyContent:'center'}}>
+                        <div style={{marginTop: '5%'}}>
                             <Image style={{ flex:1, 
                                         height: undefined, 
                                         width: undefined,
@@ -21,28 +16,30 @@ function Login () {
                         </div>
                     </Col>
                     <Col style={{backgroundColor:'skyblue'}}>
-                            <h1 style={{paddingTop:'50%'}}>
-                                welcome 2 readyPlate
-                            </h1>
-                            <Form style={{paddingTop:'5%'}}>
-                                <Form.Group controlId="formUsername">
-                                    <Form.Label>Username</Form.Label>
-                                    <Form.Control type="username" placeholder="Enter Username" />
-                                </Form.Group>
-                                <Form.Group controlId="formBasicPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" />
-                                </Form.Group>
-                                <Button style={{marginTop:'5%'}} block>
-                                        Submit
-                                </Button>
-                                <p style={{marginTop:'10%'}}>
-                                    Don't have an account yet?
-                                </p>
-                                <Button block>
-                                    Sign Up
-                                </Button>
-                            </Form>
+                        <Jumbotron style={{marginTop:'5%'}}>
+                            <Container>
+                                <h1 style={{textAlign: 'center'}}>Welcome to readyPlate!</h1>
+                            </Container>
+                        </Jumbotron>
+                        <Form style={{paddingTop:'5%', flex: 1, height: "1"}}>
+                            <Form.Group controlId="formUsername">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="username" placeholder="Enter Username" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button style={{marginTop:'5%'}} block>
+                                    Submit
+                            </Button>
+                            <p style={{marginTop:'10%'}}>
+                                Don't have an account yet?
+                            </p>
+                            <Button block>
+                                Sign Up
+                            </Button>
+                        </Form>
                     </Col>
                 </Row>
             </Container>

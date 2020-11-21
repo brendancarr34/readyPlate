df6cd0cdbe098cb6e379accdd54334646c68524d
Jack Decker <jackdeckere@gmail.com>
2020-11-21 13:11:08 -0500

    Finished school autocomplete

--- src/components/SchoolSearch.js
+++ src/components/SchoolSearch.js
@@ -1,18 +1,13 @@
 import React, { useState } from 'react';
-import { Form, ListGroup } from 'react-bootstrap';
+import { Form, ListGroup, Button } from 'react-bootstrap';
 import useSearch from '../hooks/searchHook.js';
 import { debounce } from "throttle-debounce";
 
 let SchoolSearch = () => {
+    // auto search for schools and update dropdown list respectively 
 
     const [searchValue, setSearchValue] = useState("");
-
-    const schools = useSearch(searchValue) || new Array(5).fill("");
     
-    const options = schools.slice(0, 5).map((school) => (
-        <ListGroup.Item>{school.name}</ListGroup.Item>
-    ))
-
     const updateSearchValue = (updateString) => {
         setSearchValue(updateString);
     };
@@ -21,13 +16,40 @@ let SchoolSearch = () => {
     const getHandleSearchChange = (event) => {
         const newValue = event.target.value;
         updateSearchValue(newValue);
-    }
+        updateSeletedSchool(newValue);
+    };
+
+    // dropdown list to be updated
+
+    const schools = useSearch(searchValue) || new Array(5).fill("");
+    
+    const options = schools.slice(0, 5).map((school) => (
+        <ListGroup.Item>
+            <Button variant='light'  value={school.name} onClick={(event) => {getHandleSchoolChange(event)}}>+</Button>
+            &nbsp;
+            {school.name}
+        </ListGroup.Item>
+    ))
+    
+    // code to change school when list item is clicked
+    const [selectedSchool, setSelectedSchool] = useState("");
+    console.log(selectedSchool);
+
+    const updateSeletedSchool = (updateString) => {
+        setSelectedSchool(updateString);
+    };
+
+    const getHandleSchoolChange = (event) => {
+        const newValue = event.target.value;
+        console.log(newValue);
+        updateSeletedSchool(newValue);
+    };
     
     return (
         <div>
             <Form.Group>
                 <Form.Label>University</Form.Label>
-                <Form.Control type="text" placeholder="Search Schools" onChange={(event) => {getHandleSearchChange(event)}}/>
+                <Form.Control type="text" placeholder="Search Schools" value={selectedSchool} onChange={(event) => {getHandleSearchChange(event)}}/>
             </Form.Group>
             <ListGroup>
                 {options}
